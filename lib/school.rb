0ac003db0df4053561951f2a04aafaa2acf1e285
Audra Tallman <audra.tallman@hotmail.com>
2019-03-19 21:07:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,3 @@
 class School
- attr_accessor :roster
- attr_reader :school
- 
-  def initialize(school) # creates class that can be initialized w/ name
-    @school = school
-  end
-    
- ROSTER = [] # creates roster 
- 
-  def roster=(roster)
-    @roster = roster
-    ROSTER[grade] << "name"
-    puts ROSTER
-  end
-  
-  def add_student(name, grade)
-  ROSTER
-  end
-    
-  def grade(grade)
-  puts name
-  end  
-  
-  def sort 
-  puts ROSTER.sort 
-  end
-  
+
 end 
