cdfc8e353b5b67d4194e1e58eb94d938d60ac878
Audra Tallman <audra.tallman@hotmail.com>
2019-03-19 20:47:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,16 @@
 class School
-  def initialize
+ attr_accessor :roster
+ attr_reader :name
+ 
+  def initialize(name)
+    @name = name
+  end
     
+ ROSTER = []
+ 
+ def roster=(roster)
+   @roster = roster
+   
   
   roster["grade_level"] = []
   roster["grade_level"] << "new_student"
